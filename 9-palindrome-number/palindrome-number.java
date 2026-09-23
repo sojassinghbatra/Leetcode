ed4473fcdba34a8b73a242072e0ef557fe616f43
sojassinghbatra <sojassingh.batra@torontomu.ca>
2026-09-22 23:03:22 -0400

    Time: 5 ms (83.66%) | Memory: 45.8 MB (86.61%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reversed = 0;
+        int original = x;
+        while(x>0){
+            int digit = x % 10;
+            reversed = reversed*10 + digit;
+            x = x/10;
+
+        }
+        return original == reversed;
+        
+        
+    }
+}
